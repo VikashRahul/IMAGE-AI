96706dcfc7fa076347591b8618d876214b05e78f
VikashRahul <r.vikash@iitg.ac.in>
2023-03-25 23:33:08 +0530

    first commit

---
+++ client/src/utils/index.js
@@ -0,0 +1,17 @@
+import FileSaver from 'file-saver';
+import { surpriseMePrompts } from '../constantS';
+
+export function getRandomPrompt(prompt) {
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length); //getting random index bwtween 1 to 49
+
+  const randomPrompt = surpriseMePrompts[randomIndex];
+
+  if (randomPrompt === prompt) return getRandomPrompt(prompt);
+
+  return randomPrompt;
+}
+
+export async function downloadImage(_id, photo) {
+  //using the file saver library
+  FileSaver.saveAs(photo, `download-${_id}.jpg`);
+}
