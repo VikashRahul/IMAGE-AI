1a947818aca9819813ca198481598c7465468e44
VikashRahul <88341160+VikashRahul@users.noreply.github.com>
2023-08-28 17:05:03 +0530

    Update CreatePost.jsx

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -24,7 +24,7 @@ const CreatePost = () => {
       try{
         // passing all the data our backend to then get back the response as image
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch('http://image-ai-red.vercel.app/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -56,7 +56,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('http://image-ai-red.vercel.app/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
