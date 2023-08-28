10420db96179c4f9c87c7983500f89af34ee300b
VikashRahul <88341160+VikashRahul@users.noreply.github.com>
2023-08-28 17:17:36 +0530

    Update Home.jsx

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
       setLoading(true);
   
       try {
-        const response = await fetch('http://image-ai-red.vercel.app/api/v1/post', {
+        const response = await fetch('https://image-ai-red.vercel.app/api/v1/post', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
